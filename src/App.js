f644478b6c38a0df3cee491d082e83cb014ead8d
AryanNoSensie <officearyan14@gmail.com>
2025-07-02 14:42:43 +0530

    okish

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="name">Aryan Sharma</h1>
+        <h2 className="heading">Learning CI/CD Pipeline on AWS</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
